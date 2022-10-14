4db65a5a3c4e7df5b7bd7d48ccb81c76c1bdc94e
dwang507 <davidwang334@gmail.com>
2022-10-14 15:13:37 -0700

    small changes

--- SearchEngine.java
+++ SearchEngine.java
@@ -57,13 +57,11 @@ public class SearchEngine implements URLHandler{
 class SearchMain {
         
     public static void main(String[] args) throws IOException {
-        // if(args.length == 0){
-        //     System.out.println("Missing port number! Try any number between 1024 to 49151");
-        //     return;
-        // }
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
 
-        int port = 2349;
-
-        Server.start(port, new SearchEngine());
+        Server.start(Integer.parseInt(args[0]), new SearchEngine());
     }
 }
